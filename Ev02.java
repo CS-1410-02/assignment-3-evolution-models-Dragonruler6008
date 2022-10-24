1200d5423b0c006d639030dd190ec25b022f65d8
Dragonruler6008 <dragonruler6008@gmail.com>
2022-10-23 23:01:58 -0600

    First and second iteration of code.
    Made one hated it, made 2 liked it and read much better.
    
    Co-authored-by: NSavage76 <NSavage76@users.noreply.github.com>

---
+++ Ev02.java
@@ -0,0 +1,112 @@
+//Second Iteration VERY GOOD
+import java.util.ArrayList;
+import java.util.Random;
+
+class Creature {
+    float dieChance = .05f;
+    float reproduceChance = .1f;
+
+    boolean die(float chance){
+        if(chance < dieChance) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    Creature reproduce(float chance){
+        return null;
+    }
+}
+
+class World {
+    World(long seed){
+        rng.setSeed(seed);
+    }
+    Random rng = new Random();
+
+    float spawnChance = 0.2f;
+    ArrayList<Creature> creatures = new ArrayList<Creature>();
+
+    void createCreature() {
+        float chance = rng.nextFloat();
+        if(chance < spawnChance){
+            //Spawn a creature now
+
+            //Pick type of Creature then spawn it
+
+
+        } else {
+            int index = rng.nextInt(0,creatures.size());
+            Creature c = creatures.get(index);
+
+            Creature baby = c.reproduce(rng.nextFloat());
+            if(baby != null){
+                creatures.add(baby);
+            }
+        }
+    }
+
+    void creatureRussianRoulette() {
+        float chance = rng.nextFloat();
+
+        int index = rng.nextInt(0,creatures.size());
+        Creature c = creatures.get(index);
+        boolean dead = c.die(chance);
+        if(dead){
+            creatures.remove(index);
+        }
+    }
+
+    void print() {
+        for(int i = 0; i < creatures.size(); i++){
+
+        }
+    }
+}
+
+class Snake extends Creature {
+    Snake(){
+        dieChance = .2f;
+        reproduceChance = .6f;
+    }
+    @Override
+    Creature reproduce(float chance){
+    if(chance < reproduceChance) {
+        return new Snake();
+    } else {
+        return null;
+    }
+}
+}
+
+class Dog extends Creature {
+    Dog(){
+        dieChance = .05f;
+        reproduceChance = .1f;
+    }
+    @Override
+    Creature reproduce(float chance){
+    if(chance < reproduceChance) {
+        return new Dog();
+    } else {
+        return null;
+    }
+}
+}
+
+class MainLoop {
+    public static void main(String[] args) {
+        //user available settings
+        World earth = new World(1337);
+        int turns = 6;
+        //end settings
+
+        for(int i = 0; i < turns; i++){
+            earth.createCreature();
+            earth.creatureRussianRoulette();
+        }
+        earth.print();
+        
+    }
+    
+}
---
+++ evo.java
@@ -0,0 +1,64 @@
+//First Iteration not very good
+interface Happy {
+    String happy = "";
+    String howToHappy = "";
+    boolean reproduce = false;
+
+    void calcDistToHappy();
+    void calcHappy();
+}
+
+interface Hunger extends Happy {
+    String areHungry = "";
+    String eat = "";
+
+    void displayHungerToUser();
+    void calcHunger();
+}
+ 
+interface Actions extends Hunger {
+    boolean walk = false;
+    boolean run = false;
+    boolean alive = true;
+
+    void calcWalkDist();
+    void calcRunDist();
+}
+
+class dog implements Actions{
+  
+    public void calcWalkDist() {
+        
+    }
+    public void calcRunDist(){
+
+    }
+    public void displayHungerToUser() {
+
+    }
+    public void calcHunger() {
+
+    }
+    public void calcDistToHappy() {
+
+    }
+    public void calcHappy() {
+
+    }
+    public void SetName() {
+
+    }
+    public void setBreed() {
+
+    }
+
+}
+
+    class main1 { 
+    // Driver Code
+    public static void main(String[] args)
+    {
+        dog d = new dog();
+        d.displayHungerToUser();
+    }
+}
